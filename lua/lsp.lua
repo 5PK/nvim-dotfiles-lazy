84e78c9f3a95dd5675f42f011ad83491b1541a47
5PK <1trankev@gmail.com>
2025-08-24 22:43:13 -0400

    use blink instead of nvim.cmp

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -5,54 +5,46 @@
 require('luasnip.loaders.from_vscode').lazy_load()
 
 -- nvim-cmp setup
-local cmp = require('cmp')
+-- local cmp = require('cmp')
 local luasnip = require('luasnip')
 
-cmp.setup({
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    ['<Tab>'] = cmp.mapping.select_next_item(),
-    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-  }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-  }, {
-    { name = 'buffer' },
-  }),
-})
-
--- nvim-autopairs setup
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-local cmp = require('cmp')
-
-cmp.event:on(
-  'confirm_done',
-  cmp_autopairs.on_confirm_done()
-)
+-- cmp.setup({
+--   snippet = {
+--     expand = function(args)
+--       luasnip.lsp_expand(args.body)
+--     end,
+--   },
+--   mapping = cmp.mapping.preset.insert({
+--     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+--     ['<C-f>'] = cmp.mapping.scroll_docs(4),
+--     ['<C-Space>'] = cmp.mapping.complete(),
+--     ['<C-e>'] = cmp.mapping.abort(),
+--     ['<CR>'] = cmp.mapping.confirm({ select = true }),
+--     ['<Tab>'] = cmp.mapping.select_next_item(),
+--     ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+--   }),
+--   sources = cmp.config.sources({
+--     { name = 'nvim_lsp' },
+--     { name = 'luasnip' },
+--   }, {
+--     { name = 'buffer' },
+--   }),
+-- })
+--
+-- -- nvim-autopairs setup
+-- local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+-- local cmp = require('cmp')
+--
+-- cmp.event:on(
+--   'confirm_done',
+--   cmp_autopairs.on_confirm_done()
+-- )
 
 -- LSP capabilities
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+-- local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -- Language server configurations
 local lspconfig = require('lspconfig')
-
--- C# LSP setup
--- require("lspconfig").omnisharp.setup {
---   cmd = { "/home/kevin/.local/omnisharp/OmniSharp" },
---   enable_roslyn_analyzers = true,
---   organize_imports_on_format = true,
---   capabilities = require("cmp_nvim_lsp").default_capabilities(),
--- }
 
 -- Go language server setup
 require('lspconfig').gopls.setup({
